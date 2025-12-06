cde2ecc17d4ce8deddd5f09e7b77c10fe124c73b
Mushfiqa1 <mbhuiy70@my.centennialcollege.ca>
2025-12-05 23:12:11 -0500

    Fix User model on backend

--- models/user.model.js
+++ models/user.model.js
@@ -4,29 +4,29 @@ import bcrypt from "bcryptjs";
 const userSchema = new mongoose.Schema(
   {
     name: { 
-      type: String, 
+      type: String,
       required: true 
     },
 
-    email: { 
-      type: String, 
-      required: true, 
-      unique: true 
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
 
-    password: { 
-      type: String, 
-      required: true 
+    password: {
+      type: String,
+      required: true,
     },
 
-    age: { type: Number, default: null },
-    height: { type: Number, default: null },
-    weight: { type: Number, default: null },
+    age: Number,
+    height: Number,
+    weight: Number,
   },
   { timestamps: true }
 );
 
-// Hash password before saving
+// Encrypt password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
@@ -34,7 +34,7 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-// Compare password on login
+// Compare password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
