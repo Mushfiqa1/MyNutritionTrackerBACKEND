f952f6718687eed9309e9ae79cfaa39ff2252252
Mushfiqa1 <mbhuiy70@my.centennialcollege.ca>
2025-12-05 23:07:24 -0500

    Fix User model to remove username and use name field

--- models/user.model.js
+++ models/user.model.js
@@ -1,25 +1,43 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
+    name: { 
+      type: String, 
+      required: true 
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
+
+    email: { 
+      type: String, 
+      required: true, 
+      unique: true 
     },
-    password: {
-      type: String,
-      required: true,
+
+    password: { 
+      type: String, 
+      required: true 
     },
+
+    age: { type: Number, default: null },
+    height: { type: Number, default: null },
+    weight: { type: Number, default: null },
   },
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
+// Compare password on login
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 export default User;
